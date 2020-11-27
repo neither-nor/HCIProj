335fac2db1cf1c0b7013c6fbe7d31c9bfced2efa
neither jiang <neiternor@gmail.com>
2020-11-27 23:28:54 +0800

    add door controlling (with bug)

---
+++ Assets/HCI/Scripts/DoorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject door;
+    public bool isOpening = false;
+    void Start()
+    {
+        isOpening = !isOpening;
+        Click();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        if (isOpening)
+        {
+            door.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+        }
+        else
+        {
+            door.transform.rotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));
+        }
+        isOpening = !isOpening;
+    }
+}
